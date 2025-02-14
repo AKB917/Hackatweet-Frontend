30e39e14d00781834947ef52fb99f6b8eac2d9fc
Your name <Your email>
2025-02-14 14:17:59 +0100

    homewall starting

--- components/HomeWall.js
+++ components/HomeWall.js
@@ -1,20 +1,63 @@
 import styles from '../styles/HomeWall.module.css';
-import Tweet  from './Tweet';
+import Tweet from './Tweet';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+
+
+
+
+
+
+
 
 
 function HomeWall() {
 
+  useEffect(() => {
+    fetch('http://localhost:3000/tweets')
+      .then(response => response.json())
+      .then(data => {
+        
+      });
+  }, []);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div >
-        <div className={styles.main} >
-         <h1 className={styles.home}>Home</h1>
-         <input className={styles.textcontainer} type="text" placeholder="What's up?" />
-         <button className={styles.tweetbutton}> Tweet</button>
-         </div>
+      <div className={styles.main} >
+        <h1 className={styles.home}>Home</h1>
+        <input className={styles.textcontainer} type="text" placeholder="What's up?" />
+        <button className={styles.tweetbutton}> Tweet</button>
+      </div>
 
-         <div>
-          <Tweet />
-         </div>
+      <div>
+        <Tweet />
+      </div>
 
 
     </div>
