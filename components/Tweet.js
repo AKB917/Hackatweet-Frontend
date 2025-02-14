2d28f55492ae1810fce9779a2e82f06140d4b5d0
Your name <Your email>
2025-02-14 13:38:28 +0100

    save part2

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,8 +1,31 @@
 import styles from '../styles/tweet.module.css';
 import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faTrash} from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 
 function Tweet() {
+
+  const [liked, setLiked] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+  
+
+  const handlelike = () => {
+    setLiked(!liked);
+    if (!liked) {
+      setLikeCount(likeCount + 1); 
+    } else {
+      setLikeCount(likeCount - 1); 
+    }
+  };
+
+  let iconHeart = {};
+  if (liked) {
+    iconHeart = { "color": "#e74c3c" };
+  }
+
+
 
   return (
     <div className={styles.main}>
@@ -17,9 +40,10 @@ function Tweet() {
             <p> YOU CAN'T SEE ME! </p>
             <p> #cenation </p>
         </div>
-        <div>
 
-        
+        <div>
+        <span onClick={handlelike} className={styles.like}> <FontAwesomeIcon icon={faHeart} style={iconHeart} /> ({likeCount}) </span>
+        <span className={styles.trash}><FontAwesomeIcon icon={faTrash} /></span>
         </div>
        
     </div>
